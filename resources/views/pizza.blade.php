7ca213852598a5c28029213c03e02f9ebde21242
bgrando24 <bgrando24@gmail.com>
2022-12-26 21:05:51 +1100

    Experimenting with HTML layout

--- resources/views/pizza.blade.php
+++ resources/views/pizza.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Pizza</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -22,11 +22,18 @@
     </head>
 
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen text-white bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             
 
             <div>
                 <h1>Pizzas</h1>
+                <ul class="list-group">
+                    <li>BBQ Chicken</li>
+                    <li>Pepperoni</li>
+                    <li>Margarita</li>
+                    <li>Meat Lovers</li>
+                    <li>Cheesy</li>
+                </ul>
             </div>
 
         </div>
